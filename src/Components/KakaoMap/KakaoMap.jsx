796b7490e3dbc25436d9e195edf727313768aed3
ZNINE <earthonemen@gmail.com>
2026-08-23 12:52:06 +0900

    2026-08-23-1252 - 코드 간략화 - 테스트 json 수정

--- src/Components/KakaoMap/KakaoMap.jsx
+++ src/Components/KakaoMap/KakaoMap.jsx
@@ -2,77 +2,35 @@ import { LocalBasedLoojup, getTransitRoute } from '../../Javascript/TourAPI/http
 import { useState, useEffect, useRef } from "react";
 
 export function getBusPath(route) {
-    // 해당 추천 경로의 첫 번째 버스 step
     const busStep = route.steps[0];
 
     return {
         points: busStep.path.points,
-
         start: busStep.path.points[0],
-
-        end: busStep.path.points[
-            busStep.path.points.length - 1
-        ],
-
+        end: busStep.path.points[busStep.path.points.length - 1],
         guidance: busStep.properties.guidance,
-
         vehicles: busStep.properties.vehicles,
-
         distance: busStep.properties.distance,
-
         time: busStep.properties.time
     };
 }
 
 export async function getStartToBus(startX, startY, busStartX, busStartY) {
-    const result = await getTransitRoute(
-        startX,
-        startY,
-        busStartX,
-        busStartY,
-        "walk"
-    );
-
+    const result = await getTransitRoute(startX, startY, busStartX, busStartY, "walk");
     return result;
 }
 
 export async function getBusToEnd(busEndX, busEndY, endX, endY) {
-    const result = await getTransitRoute(
-        busEndX,
-        busEndY,
-        endX,
-        endY,
-        "walk"
-    );
-
+    const result = await getTransitRoute( busEndX, busEndY, endX, endY, "walk");
     return result;
 }
 
 export async function makeSet( route, startX, startY, endX, endY ) {
-    // ① 버스 경로
     const busPath = getBusPath(route);
+    const startToBus = await getStartToBus(startX, startY, busPath.start[0], busPath.start[1]);
+    const busToEnd = await getBusToEnd(busPath.end[0], busPath.end[1], endX, endY);
 
-    // ② 출발지 → 버스 승차점
-    const startToBus = await getStartToBus(
-        startX,
-        startY,
-        busPath.start[0],
-        busPath.start[1]
-    );
-
-    // ③ 버스 하차점 → 목적지
-    const busToEnd = await getBusToEnd(
-        busPath.end[0],
-        busPath.end[1],
-        endX,
-        endY
-    );
-
-    return {
-        startToBus,
-        busPath,
-        busToEnd
-    };
+    return { startToBus, busPath, busToEnd };
 }
 
 export function KakaoMap({info, Spots}) {
@@ -92,40 +50,23 @@ export function KakaoMap({info, Spots}) {
                 const transitResult = await getTransitRoute( startX, startY, endX, endY, "publictraffic" );
                 const route = transitResult.routes[0];
 
-                
-                const pathSet =
-                    await makeSet(
-                        route,
-                        startX,
-                        startY,
-                        endX,
-                        endY
-                    );
-
+                const pathSet = await makeSet( route, startX, startY, endX, endY);
 
                 result.push(pathSet);
             }
-
-
             setPathArray(result);
         }
-
         loadRoute();
-
     }, [Spots, info.allDay]);
 
-
+    const color = ['red', 'blue', 'green'];
+    const [count, setCount] = useState(0);
     const mapRef = useRef(null);
-
     useEffect(() => {
 
         if (!window.kakao) return;
-
         window.kakao.maps.load(() => {
-
-            const map = new window.kakao.maps.Map(
-                mapRef.current,
-                {
+            const map = new window.kakao.maps.Map(mapRef.current, {
                     center: new window.kakao.maps.LatLng(
                         35.925329,
                         128.547035
@@ -138,73 +79,47 @@ export function KakaoMap({info, Spots}) {
                 return;
             }
 
-            const bounds =
-                new window.kakao.maps.LatLngBounds();
-
-            // ==========================
-            // 모든 Set을 하나씩 그림
-            // ==========================
+            const bounds = new window.kakao.maps.LatLngBounds();
 
             pathArray.forEach((set) => {
 
-                // --------------------------
-                // ① 출발지 → 버스 : 도보
-                // --------------------------
-
+                // 출발지 -> 버스
                 const startToBusPoints =
                     set.startToBus.route.legs
                         .flatMap(leg => leg.steps)
                         .flatMap(step => step.path.points);
 
-                const startToBusPath =
-                    startToBusPoints.map(([x, y]) => {
+                const startToBusPath = startToBusPoints.map(([x, y]) => {
+                    const position = new window.kakao.maps.LatLng(y, x);
+                    bounds.extend(position);
 
-                        const position =
-                            new window.kakao.maps.LatLng(y, x);
-
-                        bounds.extend(position);
-
-                        return position;
-                    });
+                    return position;
+                });
 
                 new window.kakao.maps.Polyline({
                     map: map,
                     path: startToBusPath,
-                    strokeWeight: 4,
+                    strokeWeight: 5,
                     strokeColor: "#888888",
                     strokeOpacity: 0.8,
                     strokeStyle: "solid"
                 });
 
-
-                // --------------------------
-                // ② 버스 경로
-                // --------------------------
-
-                const busPath =
-                    set.busPath.points.map(([x, y]) => {
-
-                        const position =
-                            new window.kakao.maps.LatLng(y, x);
-
+                // 버스
+                const busPath = set.busPath.points.map(([x, y]) => {
+                        const position = new window.kakao.maps.LatLng(y, x);
                         bounds.extend(position);
-
                         return position;
                     });
 
                 new window.kakao.maps.Polyline({
                     map: map,
                     path: busPath,
-                    strokeWeight: 6,
-                    strokeColor: "#2196F3",
+                    strokeWeight: 10,
+                    strokeColor: `${color[count]}`,
                     strokeOpacity: 0.9,
                     strokeStyle: "solid"
                 });
-
-
-                // --------------------------
-                // ③ 버스 → 도착지 : 도보
-                // --------------------------
 
                 const busToEndPoints =
                     set.busToEnd.route.legs
@@ -214,14 +129,11 @@ export function KakaoMap({info, Spots}) {
                 const busToEndPath =
                     busToEndPoints.map(([x, y]) => {
 
-                        const position =
-                            new window.kakao.maps.LatLng(y, x);
-
+                        const position =  new window.kakao.maps.LatLng(y, x);
                         bounds.extend(position);
-
                         return position;
                     });
-
+                
                 new window.kakao.maps.Polyline({
                     map: map,
                     path: busToEndPath,
@@ -232,13 +144,12 @@ export function KakaoMap({info, Spots}) {
                 });
 
             });
-
-            // 모든 세트가 지도에 들어오도록 영역 조정
             map.setBounds(bounds);
         });
+        let temp = count;
+        setCount(temp++);
 
     }, [pathArray]);
-
 
     return (
         <div
